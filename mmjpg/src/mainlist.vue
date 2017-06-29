8b1e9fb2c24cbf82c053fa26b999dad849cb2057
Xenon Chau <cto@ido-game.com>
2017-06-29 10:42:05 +0800

    like

--- mmjpg/src/mainlist.vue
+++ mmjpg/src/mainlist.vue
@@ -31,6 +31,8 @@
             this.rows.push('pc/mainpager/pcmaintopslider')
             this.rows.push('pc/mainhot/pcmainhotlist')
             this.rows.push('pc/mainhot/pcmainhotlist_v')
+            this.rows.push('pc/mainlike/pcmainlikelist')
+            this.rows.push('pc/mainlike/pcmainlikelist_v')
             this.rows.push('stocknews/stocknews')
             this.rows.push('components/a')
             this.rows.push('components/slider')
