653e0be7b2d08495cc56e18ed2e3d074c956413a
Xenon Chau <cto@ido-game.com>
2017-07-05 15:22:32 +0800

    看图

---
+++ zjito/src/content/pccontentlist_item_v.vue
@@ -0,0 +1,45 @@
+<template>
+        <div class="news-content" @click="todetail(stockitem.href)">
+            <text class="txt">{{stockitem.other}}</text>
+            <text class="txt">{{stockitem.title}}</text>
+            <text class="txt">{{stockitem.alt}}</text>
+            <image class="img" :src="stockitem.src"></image>
+        </div>
+</template>
+
+<script>
+    var weexEventModule = weex.requireModule('weexEventModule');
+    module.exports = {
+        created:function(){
+            console.log('news');
+        },
+
+        props: {
+            stockitem: {
+                type: Object
+            }
+        },
+
+        methods:{
+            todetail:function (e) {
+                weexEventModule.startWebViewActivity(e);
+            }
+        }
+    }
+</script>
+
+<style>
+    .news-content{
+        margin-left:1;
+        margin-right:1;
+        flex-direction:column;
+    }
+    .img{
+        width: 750;
+        height: 900;
+    }
+    .txt{
+        opacity:0.3;
+        font-size:30;
+    }
+</style>
