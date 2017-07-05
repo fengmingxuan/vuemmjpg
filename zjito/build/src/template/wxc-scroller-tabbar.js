cf339b78ae61048b85835fd40d4f3bfac91ea095
Xenon Chau <cto@ido-game.com>
2017-07-05 10:57:41 +0800

    zjito

---
+++ zjito/build/src/template/wxc-scroller-tabbar.js
@@ -0,0 +1,356 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+	__webpack_require__(132)
+	var __weex_template__ = __webpack_require__(136)
+	var __weex_style__ = __webpack_require__(137)
+	var __weex_script__ = __webpack_require__(138)
+
+	__weex_define__('@weex-component/6b47ed03eadce21bedd577ab14a4301b', [], function(__weex_require__, __weex_exports__, __weex_module__) {
+
+	    __weex_script__(__weex_module__, __weex_exports__, __weex_require__)
+	    if (__weex_exports__.__esModule && __weex_exports__.default) {
+	      __weex_module__.exports = __weex_exports__.default
+	    }
+
+	    __weex_module__.exports.template = __weex_template__
+
+	    __weex_module__.exports.style = __weex_style__
+
+	})
+
+	__weex_bootstrap__('@weex-component/6b47ed03eadce21bedd577ab14a4301b',undefined,undefined)
+
+/***/ }),
+
+/***/ 132:
+/***/ (function(module, exports, __webpack_require__) {
+
+	var __weex_template__ = __webpack_require__(133)
+	var __weex_style__ = __webpack_require__(134)
+	var __weex_script__ = __webpack_require__(135)
+
+	__weex_define__('@weex-component/wxc-scroller-tabitem', [], function(__weex_require__, __weex_exports__, __weex_module__) {
+
+	    __weex_script__(__weex_module__, __weex_exports__, __weex_require__)
+	    if (__weex_exports__.__esModule && __weex_exports__.default) {
+	      __weex_module__.exports = __weex_exports__.default
+	    }
+
+	    __weex_module__.exports.template = __weex_template__
+
+	    __weex_module__.exports.style = __weex_style__
+
+	})
+
+
+/***/ }),
+
+/***/ 133:
+/***/ (function(module, exports) {
+
+	module.exports = {
+	  "type": "div",
+	  "classList": [
+	    "container"
+	  ],
+	  "style": {
+	    "backgroundColor": function () {return this.backgroundColor}
+	  },
+	  "events": {
+	    "click": "onclickitem"
+	  },
+	  "children": [
+	    {
+	      "type": "image",
+	      "classList": [
+	        "top-line"
+	      ],
+	      "attr": {
+	        "src": "http://gtms03.alicdn.com/tps/i3/TB1mdsiMpXXXXXpXXXXNw4JIXXX-640-4.png"
+	      }
+	    },
+	    {
+	      "type": "image",
+	      "classList": [
+	        "tab-icon"
+	      ],
+	      "attr": {
+	        "src": function () {return this.icon}
+	      }
+	    },
+	    {
+	      "type": "text",
+	      "classList": [
+	        "tab-text"
+	      ],
+	      "style": {
+	        "color": function () {return this.titleColor}
+	      },
+	      "attr": {
+	        "value": function () {return this.title}
+	      }
+	    }
+	  ]
+	}
+
+/***/ }),
+
+/***/ 134:
+/***/ (function(module, exports) {
+
+	module.exports = {
+	  "container": {
+	    "flexDirection": "column",
+	    "alignItems": "center",
+	    "justifyContent": "center",
+	    "height": 88,
+	    "width": 100
+	  },
+	  "top-line": {
+	    "position": "absolute",
+	    "top": 0,
+	    "left": 0,
+	    "right": 0,
+	    "height": 2
+	  },
+	  "tab-icon": {
+	    "marginTop": 5,
+	    "width": 40,
+	    "height": 40
+	  },
+	  "tab-text": {
+	    "marginTop": 5,
+	    "textAlign": "center",
+	    "fontSize": 20
+	  }
+	}
+
+/***/ }),
+
+/***/ 135:
+/***/ (function(module, exports) {
+
+	module.exports = function(module, exports, __weex_require__){'use strict';
+
+	module.exports = {
+	    data: function () {return {
+	        index: 0,
+	        title: '',
+	        titleColor: '#000000',
+	        icon: '',
+	        backgroundColor: '#ffffff'
+	    }},
+	    methods: {
+	        onclickitem: function onclickitem(e) {
+	            var vm = this;
+	            var params = {
+	                index: vm.index
+	            };
+	            vm.$dispatch('tabItem.onClick', params);
+	        }
+	    }
+	};}
+	/* generated by weex-loader */
+
+
+/***/ }),
+
+/***/ 136:
+/***/ (function(module, exports) {
+
+	module.exports = {
+	  "type": "div",
+	  "classList": [
+	    "wrapper"
+	  ],
+	  "children": [
+	    {
+	      "type": "embed",
+	      "classList": [
+	        "content"
+	      ],
+	      "style": {
+	        "visibility": function () {return this.visibility}
+	      },
+	      "repeat": function () {return this.tabItems},
+	      "attr": {
+	        "src": function () {return this.src},
+	        "type": "weex"
+	      }
+	    },
+	    {
+	      "type": "div",
+	      "classList": [
+	        "tabbar"
+	      ],
+	      "children": [
+	        {
+	          "type": "scroller",
+	          "classList": [
+	            "scroller"
+	          ],
+	          "attr": {
+	            "scrollDirection": "horizontal"
+	          },
+	          "children": [
+	            {
+	              "type": "wxc-scroller-tabitem",
+	              "repeat": function () {return this.tabItems},
+	              "attr": {
+	                "index": function () {return this.index},
+	                "icon": function () {return this.icon},
+	                "title": function () {return this.title},
+	                "titleColor": function () {return this.titleColor}
+	              }
+	            }
+	          ]
+	        }
+	      ]
+	    }
+	  ]
+	}
+
+/***/ }),
+
+/***/ 137:
+/***/ (function(module, exports) {
+
+	module.exports = {
+	  "wrapper": {
+	    "width": 750,
+	    "position": "absolute",
+	    "top": 0,
+	    "left": 0,
+	    "right": 0,
+	    "bottom": 0
+	  },
+	  "content": {
+	    "position": "absolute",
+	    "top": 0,
+	    "left": 0,
+	    "right": 0,
+	    "bottom": 0,
+	    "marginTop": 0,
+	    "marginBottom": 88
+	  },
+	  "tabbar": {
+	    "flexDirection": "row",
+	    "position": "fixed",
+	    "bottom": 0,
+	    "left": 0,
+	    "right": 0,
+	    "height": 88,
+	    "width": 750
+	  },
+	  "scroller": {
+	    "flexDirection": "row",
+	    "height": 88
+	  },
+	  "item": {
+	    "justifyContent": "center",
+	    "borderBottomWidth": 2,
+	    "borderBottomColor": "#c0c0c0",
+	    "height": 88,
+	    "width": 100,
+	    "backgroundColor": "#00BDFF",
+	    "margin": 2
+	  }
+	}
+
+/***/ }),
+
+/***/ 138:
+/***/ (function(module, exports, __webpack_require__) {
+
+	module.exports = function(module, exports, __weex_require__){'use strict';
+
+	__webpack_require__(132);
+	module.exports = {
+	    data: function () {return {
+	        tabItems: [],
+	        selectedIndex: 0,
+	        selectedColor: '#ff0000',
+	        unselectedColor: '#000000'
+	    }},
+	    created: function created() {
+	        this.selected(this.selectedIndex);
+
+	        this.$on('tabItem.onClick', function (e) {
+	            var detail = e.detail;
+	            this.selectedIndex = detail.index;
+	            this.selected(detail.index);
+
+	            var params = {
+	                index: detail.index
+	            };
+	            this.$dispatch('tabBar.onClick', params);
+	        });
+	    },
+	    methods: {
+	        selected: function selected(index) {
+	            for (var i = 0; i < this.tabItems.length; i++) {
+	                var tabItem = this.tabItems[i];
+	                if (i == index) {
+	                    tabItem.icon = tabItem.selectedImage;
+	                    tabItem.titleColor = this.selectedColor;
+	                    tabItem.visibility = 'visible';
+	                } else {
+	                    tabItem.icon = tabItem.image;
+	                    tabItem.titleColor = this.unselectedColor;
+	                    tabItem.visibility = 'hidden';
+	                }
+	            }
+	        }
+	    }
+	};}
+	/* generated by weex-loader */
+
+
+/***/ })
+
+/******/ });
