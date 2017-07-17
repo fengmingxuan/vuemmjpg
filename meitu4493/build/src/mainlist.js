12341c93e81f1b466a43cab3ca9fb0a1da44338d
Xenon Chau <cto@ido-game.com>
2017-07-17 14:19:45 +0800

    meitu4493

---
+++ meitu4493/build/src/mainlist.js
@@ -0,0 +1,322 @@
+// { "framework": "Vue" }
+
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	var __vue_exports__, __vue_options__
+	var __vue_styles__ = []
+
+	/* styles */
+	__vue_styles__.push(__webpack_require__(1)
+	)
+
+	/* script */
+	__vue_exports__ = __webpack_require__(2)
+
+	/* template */
+	var __vue_template__ = __webpack_require__(4)
+	__vue_options__ = __vue_exports__ = __vue_exports__ || {}
+	if (
+	  typeof __vue_exports__.default === "object" ||
+	  typeof __vue_exports__.default === "function"
+	) {
+	if (Object.keys(__vue_exports__).some(function (key) { return key !== "default" && key !== "__esModule" })) {console.error("named exports are not supported in *.vue files.")}
+	__vue_options__ = __vue_exports__ = __vue_exports__.default
+	}
+	if (typeof __vue_options__ === "function") {
+	  __vue_options__ = __vue_options__.options
+	}
+	__vue_options__.__file = "/Users/master/gitweexvue/vuemmjpg/meitu4493/src/mainlist.vue"
+	__vue_options__.render = __vue_template__.render
+	__vue_options__.staticRenderFns = __vue_template__.staticRenderFns
+	__vue_options__._scopeId = "data-v-92b05c3c"
+	__vue_options__.style = __vue_options__.style || {}
+	__vue_styles__.forEach(function (module) {
+	  for (var name in module) {
+	    __vue_options__.style[name] = module[name]
+	  }
+	})
+	if (typeof __register_static_styles__ === "function") {
+	  __register_static_styles__(__vue_options__._scopeId, __vue_styles__)
+	}
+
+	module.exports = __vue_exports__
+	module.exports.el = 'true'
+	new Vue(module.exports)
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+	module.exports = {
+	  "scroller": {
+	    "borderWidth": 3,
+	    "borderStyle": "solid",
+	    "borderColor": "rgb(162,217,192)",
+	    "marginLeft": 1,
+	    "marginRight": 1
+	  },
+	  "row": {
+	    "height": 150,
+	    "flexDirection": "column",
+	    "justifyContent": "center",
+	    "paddingLeft": 30,
+	    "borderBottomWidth": 2,
+	    "borderBottomStyle": "solid",
+	    "borderBottomColor": "#DDDDDD"
+	  },
+	  "text": {
+	    "fontSize": 45,
+	    "color": "#666666"
+	  }
+	}
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+
+	var navigator = weex.requireModule('navigator');
+	var weexNavigatorModule = weex.requireModule('weexNavigatorModule');
+	var weexModule = weex.requireModule('weexModule');
+	var dom = weex.requireModule('dom');
+	var modal = weex.requireModule('modal');
+	var meitu = __webpack_require__(3);
+	exports.default = {
+	    data: function data() {
+	        return {
+	            rows: []
+	        };
+	    },
+	    created: function created() {
+	        this.rows.push('webnews');
+	        this.rows.push('template/navbar_v');
+	    },
+
+	    methods: {
+	        openitem: function openitem(event) {
+
+	            //callJS tasks:[{"data":"14","type":2},{"data":"[{\"args\":[\"150\",\"click\",{\"position\":{\"height\":52.77778,\"width\":713.19446,\"x\":33.333332,\"y\":191.66667}},null],\"method\":\"fireEvent\"}]","type":3}]
+	            // tasks:[{"module":"modal","method":"toast","args":[{"message":{"position":{"height":52.77778,"width":713.19446,"x":33.333332,"y":191.66667},"type":"click",
+	            // "target":{"ref":"186","type":"text","attr":{"value":"a"},"style":{"fontSize":45,"color":"#666666"},"event":["click"]},"timestamp":1488878471697}}]}]
+	            var name = event.target.attr.value;
+	            //                modal.toast({ message:  name.toString()})
+	            //http://localhost:8080/index.html?page=./mmjpg/build/mainlilst.js
+	            //                 weexModule.openUrl('http://192.168.1.15:8080/dist/'+name+'.weex.js', function(err){
+	            //                   console.log(err);
+	            //                  });
+	            weexNavigatorModule.push({
+	                url: meitu.getDefaultUrl(name),
+	                animated: "true"
+	            }, function (event) {
+	                // modal.toast({ message: 'callback: ' + event })
+	            });
+	        }
+	    }
+	};
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+	var BASE_URL = {
+	    //win 执行start npm run build:native  npm run build:browser  npm run serve &  npm run dev:mmjpg
+	    //raw.githubusercontent.com/fengmnegchang/vuemmjpg/master 192.168.1.15:8080 192.168.1.9:8080
+	    //https://raw.githubusercontent.com/fengmingxuan/vuemmjpg/master/mmjpg/build/src/mainlist.js
+	    IP: 'raw.githubusercontent.com/fengmingxuan/vuemmjpg/master',
+	    HTTP: 'https://',//https:// http://
+
+	};
+
+	var MEITU = {
+	    pc_meitu:"https://www.4493.com/",
+
+
+	};
+
+	exports.getpc_meitu = function () {
+	    var url = MEITU.pc_meitu;
+	    console.log('pc_meitu==' + url);
+	    return url;
+	};
+
+
+	exports.getDefaultUrl = function (name) {
+	    var url;
+	    url = getBaseUrl(name, true) + name + ".js";
+	    console.log('getDefaultUrl==' + url);
+	    return url;
+	};
+
+	exports.getDefaultPathUrl = function (path) {
+	    var url;
+	    url = getBaseUrl(path, true) + path;
+	    console.log('getPathUrl==' + url);
+	    return url;
+	};
+
+	exports.getPathUrl = function (path, isnative) {
+	    var url;
+	    url = getBaseUrl(path, isnative) + path;
+	    console.log('getPathUrl==' + url);
+	    return url;
+	};
+
+	//获取线上资源文件地址
+	exports.getImageUrl = function (path) {
+	    var url;
+	    if (typeof window === 'object') {
+	        url = BASE_URL.HTTP + BASE_URL.IP + '/meitu4493' + path.substring(1, path.length);
+	    } else {
+	        url = BASE_URL.HTTP + BASE_URL.IP + '/meitu4493' + path.substring(1, path.length);
+
+	    }
+	    console.log('getImageUrl=='+url);
+	    return url;
+	};
+
+	exports.getUrl = function (path) {
+	    var url;
+	    url = BASE_URL.HTTP+BASE_URL.IP+'/'+path;
+	    console.log('getUrl==' + url);
+	    return url;
+	};
+
+	function getBaseUrl(bundleUrl, isnav) {
+	    bundleUrl = new String(bundleUrl);
+	    var nativeBase;
+	    var isAndroidAssets = bundleUrl.indexOf('file://assets/') >= 0;
+
+	    var isiOSAssets = bundleUrl.indexOf('file:///') >= 0 && bundleUrl.indexOf('WeexDemo.app') > 0;
+	    if (isAndroidAssets) {
+	        nativeBase = 'file://assets/build/';
+	    }
+	    else if (isiOSAssets) {
+	        nativeBase = bundleUrl.substring(0, bundleUrl.lastIndexOf('/') + 1);
+	    }
+	    else {
+	        //'localhost:8080';
+	        var host = BASE_URL.IP;
+	        // var matches = /\/\/([^\/]+?)\//.exec(bundleUrl);
+	        // if (matches && matches.length >= 2) {
+	        //     host = matches[1];
+	        // }
+
+	        if (typeof window === 'object') {
+	            if (host.endsWith(':8080/meitu4493') || host.endsWith(':12580/meitu4493')) {
+	                host = host.replace('/meitu4493', '');
+	                // console.log('replace local test storm name');
+	            }
+	        }
+
+	        //此处需注意一下,tabbar 用的直接是jsbundle 的路径,但是navigator是直接跳转到新页面上的.
+	        //网页 http://localhost:8080/index.html?page=./dist/weexbar/stocknews.js
+	        //android 原生 http://192.168.1.15:12580/dist/mainlist.js
+	        if (typeof window === 'object') {
+	            nativeBase = isnav ? BASE_URL.HTTP + host + '/index.html?page=./meitu4493/build/src/' : BASE_URL.HTTP + host + '/meitu4493/build/src/';
+	        } else {
+	            nativeBase = BASE_URL.HTTP + host + '/meitu4493/build/src/';
+	            //放在官方仓库 'incubator-weex/examples/TGB_WEEX' 文件夹下编译的话，路径用这个
+	            // nativeBase = 'http://' + host.replace("8080","12580") + '/examples/build/TGB_WEEX/storm/src/';
+	        }
+	    }
+
+	    return nativeBase;
+	};
+
+
+	exports.getUrlParam = function getUrlParam(key) {
+	    var reg = new RegExp('[?|&]' + key + '=([^&]+)')
+	    var match = location.search.match(reg)
+	    return match && match[1]
+	}
+
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports) {
+
+	module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
+	  return _c('div', {
+	    staticClass: ["wrapper"]
+	  }, [_c('scroller', {
+	    staticClass: ["scroller"]
+	  }, _vm._l((_vm.rows), function(name, index) {
+	    return _c('div', {
+	      ref: 'item' + index,
+	      refInFor: true,
+	      staticClass: ["row"]
+	    }, [_c('text', {
+	      ref: 'text' + index,
+	      refInFor: true,
+	      staticClass: ["text"],
+	      on: {
+	        "click": _vm.openitem
+	      }
+	    }, [_vm._v(_vm._s(name))])])
+	  }))])
+	},staticRenderFns: []}
+	module.exports.render._withStripped = true
+
+/***/ })
+/******/ ]);
---
+++ meitu4493/src/template/navbar_v.vue
@@ -0,0 +1,219 @@
+<template>
+    <div>
+        <div class="nav_bar">
+            <div class="nav_back" @onclick="nativeback">
+                <image class="img" :src="getImgUrl(leftsrc)" v-if="shownleft"></image>
+                <!--http://192.168.1.15:8080/mmjpg/images/search.png-->
+            </div>
+            <div class="nav_title">
+                <text class="nav_text">{{title}}</text>
+                <!--<image class="imglogo" src="{{getImgUrl('../images/logo.png')}}" if="{{shownleft}}"></image>-->
+            </div>
+            <div class="nav_right_menu">
+                <div class="nav_right_menu" v-if="shown" @onclick="onright">
+                    <image class="img_menu" :src="getImgUrl('./images/search.png')"></image>
+                </div>
+            </div>
+        </div>
+        <div class="nav_line"></div>
+    </div>
+</template>
+
+<script>
+    var meitu = require('../meitu');
+    const navigator = weex.requireModule('navigator')
+
+    export default {
+        data() {
+            return {
+                title: '4493美图',
+                type: 0,
+                shown: true,
+                showStatusBar: 0,
+                nav_text_top: 0,
+                shownleft: true,
+                leftsrc: './images/menu.png'
+            }
+
+        },
+        props: ['title','shown','leftsrc'],
+        created: function () {
+            this.platform = this.$getConfig().env.platform;
+            if (this.platform == 'iOS') {
+                this.showStatusBar = 1;
+            }
+            if (typeof window === 'object') {
+                this.nav_text_top = 1;
+            } else {
+                this.nav_text_top = 0;
+            }
+            if(this.leftsrc==undefined){
+                this.leftsrc= './images/menu.png';
+            }
+            if(this.title==undefined){
+                this.title= '4493美图';
+            }
+        },
+        ready: function () {
+
+
+        },
+
+        methods: {
+            nativeback: function (e) {
+//                var params = {
+//                    'animated': 'true'
+//                };
+//                navigator.pop(params, event => {
+//
+//                });
+                this._parent.togglemenu();
+            },
+            onright: function (e) {
+                console.log('navbar == onright');
+                this._parent.onright();
+
+            },
+
+            getImgUrl: function (url) {
+                return meitu.getImageUrl(url);
+            },
+
+            setLeftImage: function (res) {
+                this.leftImage = res;
+//                console.log('navbar == res'+res);
+            },
+
+            setRightImage: function (res) {
+                this.rightImage = res;
+//                    console.log('navbar == res'+res);
+            }
+
+        }
+    }
+</script>
+
+<style>
+    .nav_bar {
+        display: flex;
+        flex-direction: row;
+        height: 100px;
+        justify-content: center;
+        align-items: center;
+        background-color: #d42591;
+    }
+
+    .nav_bar-0 {
+        background-color: #d42591;
+    }
+
+    .nav_bar-1 {
+        background-color: #d42591;
+    }
+
+    .nav_text {
+        font-size: 20wx;;
+        flex: 1;
+        justify-content: center;
+        align-items: center;
+        margin-top: 1px;
+        color: #ffffff;
+    }
+
+    .nav_text_top-0 {
+        margin-top: 1px;
+    }
+
+    .nav_text_top-1 {
+        margin-top: 1px;
+    }
+
+    .nav_text-0 {
+        color: #FFFFFF;
+    }
+
+    .nav_text-1 {
+        color: #fff;
+    }
+
+    .nav_title {
+        flex: 1;
+        margin-left: 10px;
+        margin-right: 10px;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .nav_back {
+        justify-content: center;
+        align-items: center;
+        width: 45wx;
+        height: 80;
+    }
+
+    .nav_right_menu {
+        width: 45wx;
+        height: 80;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .img {
+        width: 70;
+        height: 70;
+        margin-left: 20;
+        padding: 10;
+    }
+
+    .nav_back-0:active {
+        background-color: #000;
+    }
+
+    .nav_back-1:active {
+        background-color: #000
+    }
+
+    .nav_right_menu-0 {
+        background-color: #000;
+    }
+
+    .nav_right_menu-1 {
+        background-color: #000;
+    }
+
+    .img_menu {
+        width: 50;
+        height: 50;
+        margin-right: 20;
+    }
+
+    .imglogo{
+        width: 240;
+        flex: 1;
+        height: 50;
+    }
+    .nav_line {
+        height: 1px;
+
+    }
+
+    .nav_line-0 {
+        background-color: #D8D8D8;
+    }
+
+    .nav_line-1 {
+        background-color: #192c46;
+    }
+
+    .status_bar_ios {
+        height: 20wx;
+    }
+
+    .status_bar-0 {
+        background-color: #000;
+    }
+
+    .status_bar-1 {
+        background-color: #000;
+    }
+</style>
