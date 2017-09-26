243d6a1c042a4bc170a8c22277e7e10ca7370878
Xenon Chau <cto@ido-game.com>
2017-09-26 13:36:47 +0800

    vue uumeitu

---
+++ uumeitu/src/articlelist/pcnav_articlelist.vue
@@ -0,0 +1,219 @@
+<template>
+    <div style="background-color:rgba(0, 0, 0, .25) ">
+        <navbar_v :title="title" :shown="shown" :leftsrc="leftsrc"></navbar_v>
+        <list class="list"  loadmoreoffset="10">
+            <refresh class="refresh" @refresh="onrefresh" @pullingdown="onpullingdown" :display="refreshing ? 'show' : 'hide'">
+                <text class="indicator">下拉刷新...</text>
+            </refresh>
+            <!--<cell>-->
+                <!--<pcinterest_hlist :taghref="taghref"></pcinterest_hlist>-->
+            <!--</cell>-->
+            <cell v-for="stockitem in stockArray">
+                <pcnav_articlelist_item :stockitem="stockitem"></pcnav_articlelist_item>
+            </cell>
+            <!--<cell>-->
+                <!--<pcrecommend :taghref="taghref"></pcrecommend>-->
+            <!--</cell>-->
+            <loading class="loading" @loading="onloading" :display="showLoading">
+                <text class="indicator_loading">加载更多...</text>
+            </loading>
+        </list>
+    </div>
+</template>
+
+<script>
+    import  navbar_v from '../template/navbar_v.vue'
+    import  pcnav_articlelist_item from './pcnav_articlelist_item.vue'
+    var stream = weex.requireModule('stream');
+    var modal = weex.requireModule('modal');
+    var weexUUMeiuJsoupModule = weex.requireModule('weexUUMeiuJsoupModule');
+    var uumeitu = require('../uumeitu');
+    var utils = require('../common/utils');
+    var storage = weex.requireModule('storage');
+    var weexEventModule = weex.requireModule('weexEventModule');
+    export default{
+        components: {
+            pcnav_articlelist_item,
+            navbar_v,
+
+        },
+        props: ['taghref'],
+        data(){
+            return{
+                stockArray:[],
+                taghref:uumeitu.getpc_qingchun(),
+                pageNo: 1,
+                refreshing: false,
+                showLoading: 'hide',
+                title:"清纯美女",
+                isFirst:1,
+                shown:false,
+                leftsrc:'./images/back.png',
+//                pagenumbers:''
+
+                 
+            }
+        },
+        created: function(){
+            var self = this;
+            var ctaghref = self.$getConfig().taghref;
+            if(ctaghref!=undefined){
+                self.taghref = ctaghref;
+            }
+            var ctitle = self.$getConfig().title;
+            if(ctitle!=undefined){
+                self.title = ctitle;
+            }
+            console.log('title=='+self.title+';taghref=='+self.taghref)
+//
+            self.refresh();
+//            storage.getItem('taghref',function(s){
+//                console.log('get taghref result:'+JSON.stringify(s));
+//                var staghref = s.data;
+//                if(staghref!=undefined){
+//                    self.taghref = staghref;
+//                }
+//                console.log('taghref=='+self.taghref);
+//
+//                self.refresh();
+//
+//            });
+        },
+        mounted: function() {
+            var self = this;
+        },
+        methods:{
+            autoRefresh(event){
+                var self = this;
+                storage.getItem('taghref',function(s){
+                    console.log('get taghref result:'+JSON.stringify(s));
+                    var staghref = s.data;
+                    if(staghref!=undefined){
+                        self.taghref = staghref;
+                    }
+                    console.log('taghref=='+self.taghref);
+                    self.refresh();
+                });
+            },
+            onloading (event) {
+                this.showLoading = 'show'
+                 this.pageNo = this.pageNo+1;
+//                this.pageNo = this.pageNo+1;
+                setTimeout(() => {
+                    this.showLoading = 'hide'
+                }, 2000)
+                this.refresh();
+            },
+
+            onpullingdown (event) {
+            },
+            onrefresh (event) {
+                this.refreshing = true;
+                this.pageNo = 1;
+                setTimeout(() => {
+                    this.refreshing = false
+                }, 2000)
+                this.refresh();
+            },
+            refresh:function(){
+                var self = this;
+                self.isFirst=0;
+                if(self.taghref==undefined){
+                    self.taghref = uumeitu.getpc_qingchun();
+                }
+                var url = self.taghref;
+                if(self.pageNo==1){
+                    url = self.taghref;
+                }else{
+                    url = self.taghref + "list_1_"+self.pageNo+".html";
+//                    url = self.taghref + self.pagenumbers;
+                }
+                console.log('url==='+url);
+                var params = {
+                    url:url,
+                    pageNo: self.pageNo
+                };
+                weexUUMeiuJsoupModule.pcnavarticlelist(params,function(e){
+                    var json = JSON.parse(e);
+                    if(self.pageNo==1){
+                        self.stockArray.splice(0, self.stockArray.length);
+                    }
+                    if (json.list) {
+                        if (json.list && json.list.length > 0) {
+                            self.parseJSON(json);
+                            var paramsCache = {
+                                url:url,
+                                typename: "pcnavarticlelist"+self.pageNo,
+                            };
+                            weexEventModule.saveCache(paramsCache,json,function(ee){
+
+                            });
+                        }else{
+                            //异常
+                            console.log('异常====');
+                            //获取缓存
+                            var paramsCache = {
+                                url:url,
+                                typename: "pcnavarticlelist"+self.pageNo
+                            };
+                            weexEventModule.queryCache(paramsCache,function(e){
+                                console.log('queryCache=='+e);
+                                var json = JSON.parse(e);
+                                self.parseJSON(json);
+                            });
+                        }
+                    }
+
+
+                });
+            },
+            parseJSON:function (json) {
+                var self = this;
+                for (var i = 0; i < json.list.length; i+=2) {
+//                    self.pagenumbers = json.list[0].pagenumbers;
+                    var tag = json.list[i];
+                    var tag2 = json.list[i+1];
+                    var item={
+                        href:tag.href,
+                        alt:tag.alt,
+                        src:tag.src,
+                        other:tag.other,
+                        title:tag.title,
+                        href2:tag2.href,
+                        alt2:tag2.alt,
+                        src2:tag2.src,
+                        other2:tag2.other,
+                        title2:tag2.title,
+                    };
+                    self.stockArray.push(item);
+                }
+            },
+
+
+        }
+
+    }
+</script>
+
+<style>
+    .refresh-view{
+        height:100;
+        width:750;
+        align-items:center;
+    }
+    .indicator {
+        color: #888888;
+        font-size: 42px;
+        text-align: center;
+    }
+    .loading {
+        justify-content: center;
+    }
+    .indicator_loading {
+        color: #888888;
+        font-size: 42px;
+        padding-top: 20px;
+        padding-bottom: 20px;
+        text-align: center;
+    }
+</style>
---
+++ uumeitu/src/uumeitu.js
@@ -0,0 +1,118 @@
+var BASE_URL = {
+    //win 执行start npm run build:native  npm run build:browser  npm run serve &  npm run dev:mmjpg
+    //raw.githubusercontent.com/fengmnegchang/vuemmjpg/master 192.168.1.15:8080 192.168.1.9:8080
+    //https://raw.githubusercontent.com/fengmingxuan/vuemmjpg/master/mmjpg/build/src/mainlist.js
+    //http://git.oschina.net/fengmengchang/weex-mmjpg
+    //http://git.oschina.net/fengmengchang/weex-mmjpg/raw/master/meitu4493/build/src/mainlist.js
+    IP: 'https://raw.githubusercontent.com/fengmingxuan/vuemmjpg/master',
+    HTTP: 'https://',//https:// http://
+
+};
+
+var UUMEITU = {
+    pc_uumeitu:"http://www.uumeitu.com/",
+    pc_qingchun:"http://www.uumeitu.com/qingchun/"
+};
+
+exports.getpc_qingchun = function () {
+    var url = UUMEITU.pc_qingchun;
+    console.log('pc_qingchun==' + url);
+    return url;
+};
+
+exports.getpc_uumeitu = function () {
+    var url = UUMEITU.pc_uumeitu;
+    console.log('pc_uumeitu==' + url);
+    return url;
+};
+
+
+exports.getDefaultUrl = function (name) {
+    var url;
+    url = getBaseUrl(name, true) + name + ".js";
+    console.log('getDefaultUrl==' + url);
+    return url;
+};
+
+exports.getDefaultPathUrl = function (path) {
+    var url;
+    url = getBaseUrl(path, true) + path;
+    console.log('getPathUrl==' + url);
+    return url;
+};
+
+exports.getPathUrl = function (path, isnative) {
+    var url;
+    url = getBaseUrl(path, isnative) + path;
+    console.log('getPathUrl==' + url);
+    return url;
+};
+
+//获取线上资源文件地址
+exports.getImageUrl = function (path) {
+    var url;
+    if (typeof window === 'object') {
+        url = BASE_URL.HTTP + BASE_URL.IP + '/uumeitu' + path.substring(1, path.length);
+    } else {
+        url = BASE_URL.HTTP + BASE_URL.IP + '/uumeitu' + path.substring(1, path.length);
+
+    }
+    console.log('getImageUrl=='+url);
+    return url;
+};
+
+exports.getUrl = function (path) {
+    var url;
+    url = BASE_URL.HTTP+BASE_URL.IP+'/'+path;
+    console.log('getUrl==' + url);
+    return url;
+};
+
+function getBaseUrl(bundleUrl, isnav) {
+    bundleUrl = new String(bundleUrl);
+    var nativeBase;
+    var isAndroidAssets = bundleUrl.indexOf('file://assets/') >= 0;
+
+    var isiOSAssets = bundleUrl.indexOf('file:///') >= 0 && bundleUrl.indexOf('WeexDemo.app') > 0;
+    if (isAndroidAssets) {
+        nativeBase = 'file://assets/build/';
+    }
+    else if (isiOSAssets) {
+        nativeBase = bundleUrl.substring(0, bundleUrl.lastIndexOf('/') + 1);
+    }
+    else {
+        //'localhost:8080';
+        var host = BASE_URL.IP;
+        // var matches = /\/\/([^\/]+?)\//.exec(bundleUrl);
+        // if (matches && matches.length >= 2) {
+        //     host = matches[1];
+        // }
+
+        if (typeof window === 'object') {
+            if (host.endsWith(':8080/uumeitu') || host.endsWith(':12580/uumeitu')) {
+                host = host.replace('/uumeitu', '');
+                // console.log('replace local test storm name');
+            }
+        }
+
+        //此处需注意一下,tabbar 用的直接是jsbundle 的路径,但是navigator是直接跳转到新页面上的.
+        //网页 http://localhost:8080/index.html?page=./dist/weexbar/stocknews.js
+        //android 原生 http://192.168.1.15:12580/dist/mainlist.js
+        if (typeof window === 'object') {
+            nativeBase = isnav ? BASE_URL.HTTP + host + '/index.html?page=./uumeitu/build/src/' : BASE_URL.HTTP + host + '/uumeitu/build/src/';
+        } else {
+            nativeBase = BASE_URL.HTTP + host + '/uumeitu/build/src/';
+            //放在官方仓库 'incubator-weex/examples/TGB_WEEX' 文件夹下编译的话，路径用这个
+            // nativeBase = 'http://' + host.replace("8080","12580") + '/examples/build/TGB_WEEX/storm/src/';
+        }
+    }
+
+    return nativeBase;
+};
+
+
+exports.getUrlParam = function getUrlParam(key) {
+    var reg = new RegExp('[?|&]' + key + '=([^&]+)')
+    var match = location.search.match(reg)
+    return match && match[1]
+}
